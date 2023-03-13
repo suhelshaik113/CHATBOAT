338dd26c3b7cb7c1b19af00d40918e188f96b2e2
suhelshaik113 <121938905+suhelshaik113@users.noreply.github.com>
2023-03-13 17:11:29 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://secnod-ai.onrender.com', {
+    const response = await fetch('https://chatbot-m8ys.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
